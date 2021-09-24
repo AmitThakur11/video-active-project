45302fc2eafccf9856cca7d23361312d75739d19
amit thakur <amithakur081@gmail.com>
2021-09-24 19:51:24 +0530

    UPDATE : login and register page

--- src/Pages/Signup/Signup.jsx
+++ src/Pages/Signup/Signup.jsx
@@ -1,9 +1,30 @@
-import "./Signup.css"
+import "../../Pages/Login/style.css"
+import SignupImg from "../../media/signUpImg.svg"
+import {useAuth} from "../../context/authContext/index"
+const Signup = () => {
+  const {getInput} = useAuth()
 
-const Signup = ()=>{
-    return(
-        <section>Signup</section>
-    )
-}
+  return (
+    <div className = "loginContainer">
+      <div className ="loginBox">
+        
+        <img src ={SignupImg} alt ="/"/>
+        <div className ="loginBox_input">
+        <div className ="loginBox_inner">
+        <div style={{fontSize:"20px", fontWeight:"600"}}>Register</div>
+        <input name = "username" placeholder="-Name"  onChange = {(e)=>getInput(e)}/>
+        <input name = "email" placeholder="-Email"  onChange = {(e)=>getInput(e)} />
+        <input name = "password" type ="password"  placeholder="-Password"  onChange = {(e)=>getInput(e)}/>
+        <input name = "cpassword" type ="password" placeholder="-Confirm password"  onChange = {(e)=>getInput(e)} />
+        <button  className="loginBox_btn">Register</button>
+        
+        </div>
+        </div>
 
-export default Signup
+      </div>
+    </div>
+  )
+   
+};
+
+export default Signup;
