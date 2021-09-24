a2d6b980ada465bb933ba7983a3923e3bf8ab21b
amit thakur <amithakur081@gmail.com>
2021-09-24 19:46:13 +0530

    UPDATE : home  page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,17 +1,29 @@
 import HomeCard from "../../Components/Cards/HomeCard/HomeCard"
 import "./Home.css";
+import {
+  RiSearchLine
+} from "react-icons/ri";
 import {Link} from "react-router-dom"
+import {useData} from "../../context/dataContext/index"
 const Home =()=>{
+  const {videoList} = useData()
     return(
+      <>
+      {/* <div className="search-bar">
+        <input placeholder="search videos" />
+        <RiSearchLine className="icon" />
+      </div> */}
       <section className ="home-container">
+        
       <section className ="home">{
-        Array(50).fill(0).map(()=>{
-          return <Link to="/video"><HomeCard/></Link>
+        videoList.map((video)=>{
+          return <Link key={video._id} to={`/video/${video._id}`}><HomeCard video = {video}/></Link>
         })
         
         }
       </section>
       </section>
+      </>
     )
   }
   
