cf9e7a19f364e3854a9aee89c53223650db0e446
amit thakur <amithakur081@gmail.com>
2021-09-24 19:44:13 +0530

    ADD :  routing

--- src/Pages/Content/Content.jsx
+++ src/Pages/Content/Content.jsx
@@ -1,12 +1,29 @@
 import "./Content.css";
+import {Routes , Route} from "react-router-dom"
 import SideBar from "../../Components/SideBar/SideBar";
-import Home from "../Home/Home"
+import Home from "../Home/Home";
+import LikedVideos from "../LikedVideos/LikedVideos";
+import History from "../History/History";
+import Playlist from "../Playlist/Playlist";
+import Login from "../Login/Login";
+import Signup from "../Signup/Signup";
+import PlayVideo from "../PlayVideo/PlayVideo";
+
 const Content = () => {
   return (
     <section className="content-container">
       <SideBar />
       <div className="content">
-        <Home/>
+        <Routes>
+          <Route path = "/" element ={<Home/>}></Route>
+          <Route path ="/likedvideos" element={<LikedVideos/>}/>
+          <Route path ="/history" element = {<History/>}/>
+          <Route Path = "/playlist" element = {<Playlist/>}/>
+          <Route path = "video/:id" element={<PlayVideo/>}/>
+          <Route path = "/login" element={<Login/>}/>
+          <Route path ="/signup" element ={<Signup/>}/>
+        </Routes>
+        
       </div>
     </section>
   );
