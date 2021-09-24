ff8a3a59d3c4774f8fcd9bb045769cc2983d6290
amit thakur <amithakur081@gmail.com>
2021-09-24 19:50:24 +0530

    ADD :  add defaults to axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./styles.css"
 import Header from "./Components/Header/Header"
 import Content from "./Pages/Content/Content"
+import {useEffect} from "react"
+import axios from "axios"
 function App() {
+  useEffect(()=>{
+    (()=>{
+      axios.defaults.baseURL = "http://localhost:5000/"
+      axios.defaults.headers.common["Authorization"] = localStorage.getItem('token');
+    })()
+
+  },[])
   return (
     <div className="App">
       <Header/>
