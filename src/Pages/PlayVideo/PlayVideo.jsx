b11c424b1a75abd641b9f55b9824ddb11252f56f
amit thakur <amithakur081@gmail.com>
2021-09-24 19:47:03 +0530

    UPDATE : video player

--- src/Pages/PlayVideo/PlayVideo.jsx
+++ src/Pages/PlayVideo/PlayVideo.jsx
@@ -1,8 +1,36 @@
 import "./PlayVideo.css"
+import Comment from "../../Components/Comment/Comment"
+import VideoFooter from "../../Components/VideoFooter/VideoFooter"
+import { useParams } from "react-router";
+import ReactPlayer from "react-player";
+import { useData } from "../../context/dataContext/index"
 
-const PlayVideo =()=>{
-    return(
-        <section>PlayVideo</section>
+
+const PlayVideo = () => {
+    const { id } = useParams()
+    const { videoList } = useData()
+    
+    return (
+        <section className="play-video-container">
+            <div className="video-container">
+                {
+                    videoList.map((video) => {
+                        return <>
+                        { video._id === id && <div className="video">
+                            <ReactPlayer url={video.url} width = "100%" height="100%"  playing  light={false} controls />
+                            </div>}
+                            </>
+
+
+                        
+                    })
+                }
+
+                <VideoFooter  videoId = {id}/>
+            </div>
+            <Comment />
+
+        </section>
     )
 }
 
