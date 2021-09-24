77c996b325836535df270c11567803263bd27c2f
amit thakur <amithakur081@gmail.com>
2021-09-24 19:49:26 +0530

    UPDATE : thubnail card

--- src/Components/Cards/HomeCard/HomeCard.jsx
+++ src/Components/Cards/HomeCard/HomeCard.jsx
@@ -1,24 +1,24 @@
 import "./HomeCard.css";
 
-const HomeCard = () => {
+const HomeCard = ({video}) => {
   return (
     <section className="home-video-card">
       <div className="hv-thumbnail">
         <img
-          src="https://i.ytimg.com/vi_webp/K92x1YUwTrY/maxresdefault.webp"
+          src={video.thumbnail}
           alt="/"
         />
       </div>
       <div className="hv-author-img">
         <img
-          src="https://yt3.ggpht.com/ytc/AKedOLSUZkxqm8jM-pksxqglNSrj26OSdp9xWruAZymDSQ=s88-c-k-c0x00ffffff-no-rj"
+          src={video.creatorUrl}
           alt="/"
         />
       </div>
       <div className="hv-title">
-        This is the Most Affordable Foldable Flagship Phone !
+        {video.title}
       </div>
-      <div className="hv-name">Tech burner</div>
+      <div className="hv-name">{video.creator}</div>
     </section>
   );
 };
