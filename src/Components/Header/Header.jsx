4fb1cc991f68baddd542dbd5ced688fc969081f5
amit thakur <amithakur081@gmail.com>
2022-01-03 02:45:13 +0530

    feat : search bar

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,13 +1,17 @@
 import "./Header.css";
-
+import {useState} from "react"
 import Logo from "../../Media/logo.png";
 import {Link} from "react-router-dom"
 
 import { useAuth } from "../../Context/authContext";
-
-
+import {useData} from "../../Context/dataContext"
 const Header = () => {
-  const {isLogin} = useAuth()
+  const {isLogin} = useAuth();
+  const {user} = useData();
+  const [search ,setSearch] = useState("");
+  const filteredData = user.videoList.filter(data => data.title.toLowerCase().includes(search.toLowerCase()))
+  console.log(filteredData)
+  
   return (
     <header>
       <Link to="/">
@@ -21,7 +25,23 @@ const Header = () => {
       </div>
       </Link>
       
+      <div className ="searchBar__section">
+        <input value ={search} className ="searchInput" placeholder ="Search" onChange = { (e)=>setSearch(e.target.value)} />
+        {search !=="" && <div className ="searchResult" onClick = {()=>setSearch("")}>
+          {
+            filteredData.length !== 0 ? filteredData.map(({title,_id})=>{
+              return <>
+              <div key ={_id} className ="searchResult__item" >
+                <Link to={`/video/${_id}`}>{title}</Link>
+                </div>
+              </>
+            }): <div className="searchResult__item">no result</div>
+          }
+
+        </div>}
+      </div>
       <div className="header-menu">
+      
       <Link to="/login" ><div className ="login-btn">{ isLogin? "Log out" :"Log in"}</div></Link>
       </div>
     </header>
